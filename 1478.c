5e72ab587be20789677ec8620607c9e4ec6f7b20
Wickhye <102145519+Wickhye@users.noreply.github.com>
2022-07-29 09:40:33 +0900

    Update 1478.c

--- 1478.c
+++ 1478.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("Hello World!");
-	return 0;
+
+int main(){
+
+    int n,m,in=1;
+    int a[105][105];
+    scanf("%d %d",&n ,&m);
+    int curve=n+m;
+
+    for(int x=0; x<curve ;x++){
+            for(int i=0; i<n; i++){
+				for(int j=0; j<m; j++){
+					if(i+j == x)a[i][j]=in++;
+				}
+			}
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=m-1; j>=0; j--) {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
 }
